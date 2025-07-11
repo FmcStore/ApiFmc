c58bd9e30288efed622b3a8a8d1a3170f0faadfa
FmcStore <guardianmarx@gmail.com>
2025-07-11 18:06:06 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -1,15 +1,15 @@
 const options = {
   name: "HitamAPIs",
-  developer: "@sayuran.vip",
+  developer: "@FmcDevloper",
   port: 3000,
-  webName: "HitamAPIs Playground",
+  webName: "FmcApi Playground",
   description: "Simple Base Rest API",
   favicon:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
 };
 
 const host = {
-  BASE_URL: "https://Apifmc.com", // Ganti dengan URL yang sesuai
+  BASE_URL: "https://api-fmc.vercel.app", // Ganti dengan URL yang sesuai
   // Contoh: https://domain.com
 };
 
